94ed8704e7c92c8c7f74b9dcbf0eb21c59b45301
Osher Demeke <324704451@idf.il>
2020-12-18 21:58:31 +0200

    refactore 1

--- medical-helpe-line/client/src/components/LogIn.tsx
+++ medical-helpe-line/client/src/components/LogIn.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+
 import {
   CssBaseline,
   TextField,
@@ -7,38 +10,61 @@ import {
   Checkbox,
   Typography,
   Container,
+  Grid,
+  InputLabel,
+  FormControl,
+  Input,
 } from "@material-ui/core";
 
 const Login = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    //   const { userName, password } = event.target.value;
+    //   setUserName(userName);
+    //   setPassword(password);
+    // };
+  };
+
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <Typography component="h1" variant="h5">
         Adnin Login
       </Typography>
-      <form noValidate>
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="user"
-          label="user name"
-          name="user name"
-          autoComplete="user name"
-          autoFocus
-        />
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-        />
+
+      <form noValidate onSubmit={handleSubmit}>
+        <Grid item sm={3}>
+          <FormControl fullWidth>
+            <InputLabel shrink>user name</InputLabel>
+            <Input
+              autoComplete="off"
+              autoFocus
+              fullWidth
+              id="user_Name"
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
+              value={userName}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item sm={3}>
+          <FormControl fullWidth>
+            <InputLabel shrink>password</InputLabel>
+            <Input
+              autoComplete="off"
+              fullWidth
+              id="password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              value={password}
+            />
+          </FormControl>
+        </Grid>
         <FormControlLabel
           control={<Checkbox value="remember" color="primary" />}
           label="Remember me"
