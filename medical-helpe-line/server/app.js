c7b9344dec3a2c94e1393f7f10cb6d740f36f007
Osher Demeke <324704451@idf.il>
2020-12-18 20:02:50 +0200

    some code

---
+++ medical-helpe-line/server/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const app = express();
+const port = 4000;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  })
+);
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public", "build")));
+app.get("/", (req, res) => {
+  res.status(200).send("hello admin");
+});
+app.post("/login", (req, res) => {
+  const { userName, password } = req.body;
+
+  console.log("req.body", req.body);
+
+  if (userName === "abc" && password === "123") {
+    return res.send({ message: "ok" });
+  }
+
+  res.sendStatus(403);
+});
+
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
